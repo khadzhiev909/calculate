445090a95af78ee11e9ff0dec3668b4159956d21
Эпенди <khadzhiev909@gmail.com>
2023-04-30 00:11:24 +0300

    т

--- src/main/java/proskydemo/CalculatorServise.java
+++ src/main/java/proskydemo/CalculatorServise.java
@@ -4,5 +4,39 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorServise implements CalculateServiseI {
+    @Override
+    public String sayHello() {
+        return "Добро пожаловать в калькулятор";
+    }
+    @Override
+    public String sum(Integer num1, Integer num2) {
+        if (num1 == null || num2 == null) {
+            return "Ошибка введите числа больше 0";
+        }
+        return "Сумма равна: " + num1 + num2;
+    }
+    @Override
+    public String minus(Integer number1, Integer number2) {
+        if (number1 == null || number2 == null) {
+            return "Ошибка введите числа больше 0";
+        } else {
+            return "Cумма равна: " + (number1 - number2);
+        }
+    }
+    @Override
+    public String multiplay(Integer number1, Integer number2) {
+        if (number1 == null && number2 == null) {
+            return "Ошибка введите числа больше 0";
+        }
+        return " " + number1 * number2;
+
+    }
+    @Override
+    public String devide(Integer number1, Integer number2) {
+        if (number1 == null && number2 == null) {
+            return "Ошибка введите числа больше 0";
+        }
+        return " " + number1 / number2;
+    }
 
 }
